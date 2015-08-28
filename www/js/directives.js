0797d7c38fd8d20a484b5c870b057458acf767f1
tresDaniel <tres.daniel@hotmail.com>
2015-08-28 18:14:31 -0300

    mostrando marker.

--- www/js/directives.js
+++ www/js/directives.js
@@ -1,6 +1,6 @@
 angular.module('starter.directives', [])
 
-.directive('map', function() {
+.directive('map', function($compile) {
   return {
     restrict: 'E',
     scope: {
@@ -13,51 +13,92 @@ angular.module('starter.directives', [])
         var mapOptions = {
           center: myLatlng,
           zoom: 16,
+          zoomControl: false,
+          panControl: false,
+          scaleControl: false,
+          streetViewControl: false,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         };
         var map = new google.maps.Map(document.getElementById("map"),
             mapOptions);
-        
+
+        var service = new google.maps.places.PlacesService(map);
+        service.nearbySearch({
+          location: myLatlng,
+          radius: 500,
+          types: ['store']
+        }, callback);
+
+        function callback(results, status) {
+          if (status === google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+              createMarker(results[i]);
+            }
+          }
+        }
+
+        function createMarker(place) {
+          var placeLoc = place.geometry.location;
+          var marker = new google.maps.Marker({
+            map: map,
+            position: place.geometry.location
+          });
+
+          google.maps.event.addListener(marker, 'click', function() {
+            infowindow.setContent(place.name);
+            infowindow.open(map, this);
+          });
+        }
+
+        function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+          infoWindow.setPosition(pos);
+          infoWindow.setContent(browserHasGeolocation ?
+                                'Error: The Geolocation service failed.' :
+                                'Error: Your browser doesn\'t support geolocation.');
+        }
+                
         //Marker + infowindow + angularjs compiled ng-click
         var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
         var compiled = $compile(contentString)($scope);
 
-        var infowindow = new google.maps.InfoWindow({
+        var infoWindow = new google.maps.InfoWindow({
+          map: map,
           content: compiled[0]
         });
 
-        var marker = new google.maps.Marker({
-          position: myLatlng,
-          map: map,
-          title: 'Uluru (Ayers Rock)'
-        });
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+              var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+              };
 
-        google.maps.event.addListener(marker, 'click', function() {
-          infowindow.open(map,marker);
-        });
+              infoWindow.setPosition(pos);
+              map.setCenter(pos);
+
+              var marker = new google.maps.Marker({
+                position: pos,
+                map: map,
+                title: 'Uluru (Ayers Rock)'
+              });
+
+              google.maps.event.addListener(marker, 'click', function() {
+                infowindow.open(map, this);
+              });
+            }, function() {
+              handleLocationError(true, infoWindow, map.getCenter());
+            });
+          } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
+          }
 
         $scope.map = map;
       }
 
       google.maps.event.addDomListener(window, 'load', initialize);
       
-      $scope.centerOnMe = function() {
-        if(!$scope.map) {
-          return;
-        }
 
-        $scope.loading = $ionicLoading.show({
-          content: 'Getting current location...',
-          showBackdrop: false
-        });
-
-        navigator.geolocation.getCurrentPosition(function(pos) {
-          $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-          $scope.loading.hide();
-        }, function(error) {
-          alert('Unable to get location: ' + error.message);
-        });
-      };
       
       $scope.clickTest = function() {
         alert('Example of infowindow with ng-click')
